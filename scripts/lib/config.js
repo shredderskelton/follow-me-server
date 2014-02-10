4b6452a35135f3474013585b9ab3c947a7f14fc3
Shredder <nick@fetchtv.com.au>
2014-02-10 19:00:50 +1100

    removed ignored folder node_modules

--- scripts/lib/config.js
+++ scripts/lib/config.js
@@ -1,9 +1,9 @@
+module.exports = {
+  host: 'polar-escarpment-6759.herokuapp.com',
+  port: 80
+};
+
 // module.exports = {
-//   host: 'polar-escarpment-6759.herokuapp.com',
+//   host: 'localhost',
 //   port: 3000
 // };
-
-module.exports = {
-  host: 'localhost',
-  port: 3000
-};
