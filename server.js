30ed897202632229905d9f34fba42aaf2483f44d
Nick Skelton <nick.skelton@sixt.com>
2016-06-28 16:31:29 +0200

    all in one

--- server.js
+++ server.js
@@ -9,10 +9,11 @@
 // client = redis.createClient();
 
 var express = require('express');
+var http = require('http');
 var app = express();
 var logfmt = require("logfmt");
 var fs = require('fs');
-var mqtt = require('./mosca-server.js')
+// var mqtt = require('./mosca-server.js')
 
 app.use(express.bodyParser());
 app.use(logfmt.requestLogger());
@@ -25,21 +26,7 @@ app.use(function(req, res, next){
   console.log('body = ' + JSON.stringify(req.body));
   next();
 });
- 
-app.param('groupId', function(req, res, next, id){
-
-  //TODO put some validation in
-  console.log('validation id = ' + id);
-  next();
-
-   // if(groupId === groupId){ 
-      // req.groupId = groupId;
-      // next();
-    // } else {
-    //   next(new Error('failed to load user'));
-    // } 
-});
-
+  
 app.get('/me', function(req, res) {
   console.log("request");
   fs.readFile("me", function(err, text) {
@@ -64,111 +51,48 @@ app.get('/test.html', function(req, res) {
   });
 });
 
-app.get('/group/:groupId', function(req, res){
+var port = Number(process.env.PORT || 3000);
+app.server = http.createServer(app);
 
-  var groupPins = groups[req.params.groupId];
+//MQTT
 
-  var ret = {
-    group : req.params.groupId,
-    members : groupPins
-  };
+var mosca = require('mosca')
+var mqttServer = new mosca.Server({});
+mqttServer.attachHttpServer(app.server);
 
-  console.log('returning group with id = ' + req.params.groupId);
-  res.setHeader('Content-Type', 'application/json');
-  res.json(ret);
-});
-
-app.get('/search/:groupId', function(req, res){
-
-  //build names for all groups 
-  var groupKeys = [];
-  for (var key in groups) {
-      if (groups.hasOwnProperty(key)) {
-          groupKeys.push(key);
-      }
-  }
-
-  //find matches
-  var matches = [];
-  var idx=-1;
-  
-  for(var i=0; i<groupKeys.length; i++){
-    idx = groupKeys[i].toLowerCase().indexOf(req.params.groupId);
-    if(idx===0)
-      matches.push(groupKeys[i]);
-  }
+mqttServer.on('ready', setup);
  
-
-  console.log('returning ' + matches.length + ' matches for: ' + req.params.groupId);
-  res.setHeader('Content-Type', 'application/json');
-  res.json(matches);
-});
-
-app.delete('/group/:groupId/user/:username', function(req, res){
-  var group = groups[req.params.groupId];
-  if(typeof group === 'undefined'){
-    console.log('group ' + req.params.groupId + ' not found');
-    res.status(404).send('Sorry cant find group!');
+// Accepts the connection if the username and password are valid
+var authenticate = function(client, username, password, callback) {
+  var shouldAuthenticate = false;
+  if(process.env.MQTT_USERNAME) { 
+    shouldAuthenticate = true; 
   }
-
-  var members = group.members;
-  var index = -1;
-  for(var i=0;i<members.length;i++){
-    if(members[i].name.valueOf() == req.params.username.valueOf()){
-      index = i;
-    }
+  if(shouldAuthenticate){
+    console.log('Authenticating');
+    var authorized = (username === process.env.MQTT_USERNAME && password.toString() === process.env.MQTT_PASSWORD);
+    if (authorized) client.user = username;
+    callback(null, authorized);
+  }else{
+    callback(null, true);
   }
-
-  if (index > -1) {
-    members.splice(index, 1);
-    res.status(200).send('User deleted from group');
-  }
-  else{
-    res.status(404).send('Sorry cant find that user!');
-  }
-});
-
-app.post('/group/:groupId/user/:username', function(req, res){
-
-  console.log('group id = ' + req.params.groupId);
-  console.log('username = ' + req.params.username);
-  console.log('body = ' + JSON.stringify(req.body));
-
-  var group = groups[req.params.groupId];
-
-  if(typeof group === 'undefined'){
-    console.log('Created group');
-    group = {};
-    group.members = [];
-    group.group = req.params.groupId;
-    groups[req.params.groupId] = group;
-  }
-
-  var members = group.members;
-  var found = false;
-  for(var i=0;i<members.length;i++){
-    if(members[i].name.valueOf() == req.params.username.valueOf()){
-      members[i] = req.body;
-      found = true;
-      break;
-    }
-  }
-
-  if(!found){
-    console.log(req.params.username + ' not found in group');
-    members.push(req.body);
-  }
-
-  res.setHeader('Content-Type', 'application/json'); 
-  console.log('sendingback = ' + JSON.stringify(group));
-  res.json(group);
-});
-
-function Follower(){
-
 }
 
-var port = Number(process.env.PORT || 3000);
-app.listen(port, function() {
+mqttServer.on('clientConnected', function(client) {
+    console.log('client connected', client.id);     
+});
+ 
+mqttServer.on('published', function(packet, client) {
+  console.log('Published', packet.payload);
+});
+ 
+function setup() {
+  mqttServer.authenticate = authenticate;
+  console.log('Mosca server is up and running')
+}
+
+app.server.listen(3000 , function() {
   console.log("Listening on " + port);
 }); 
+
+
