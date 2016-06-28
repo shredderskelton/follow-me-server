1381ab4656bf4f8acf5ba4904d1325a08db5a70d
Nick Skelton <nick.skelton@sixt.com>
2016-06-28 16:18:28 +0200

    try web sockets

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ function Follower(){
 
 }
 
-var port = Number(process.env.PORT || 3001);
+var port = Number(process.env.PORT || 3000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 }); 
